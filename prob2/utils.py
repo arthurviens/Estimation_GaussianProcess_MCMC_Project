b17eb42b1267465c9ba9fbe3b9962c6275627756
Arthur Viens <arthur.viens@mines-paristech.fr>
2022-01-04 17:31:05 +0100

    Avancement, quelques essais mais ne fonctionne pas

--- prob2/utils.py
+++ prob2/utils.py
@@ -16,6 +16,8 @@ def sigmaKov(X, lbda, func):
         valxi = X[i]
         for j in range(sh):
             sig[i, j] = func(lbda, valxi, X[j])
+    
+    sig = sig + np.diag(np.random.uniform(low = 0., high = 0.000001, size=sh))
     assert sig.shape == (sh, sh), f"Covariance matrix has a wrong shape {sig.shape} instead of {(sh, sh)}" 
     return sig
 
@@ -23,14 +25,14 @@ def sigmaKov(X, lbda, func):
 def simulGP_gauss(X, N, lbda):
     sigma = sigmaKov(X, lbda, kov)
     L = np.linalg.cholesky(sigma)
-    Lt = L.T
+    #Lt = L.T
     fig, ax = plt.subplots()
     plt.xlabel("X")
     plt.ylabel("Valeur de la variable aléatoire")
     plt.title(r"Réalisations de GP pour $\lambda$ = " + str(lbda))
     for i in range(5):
         g = np.random.normal(0, 1, size=N)
-        z = Lt @ g 
+        z = L @ g 
         #display(z.round(2))
         ax.plot(X, z, label = f"Sim {i}")
     plt.legend()
@@ -39,19 +41,22 @@ def simulGP_gauss(X, N, lbda):
 
 
 def simulGP_sin(X, N, lbda):
-    sigma = sigmaKov(X, lbda, kov)
+    X_random = draw_uniform_in_intervals(N+1)
+    Xsin = np.sin(4*np.pi*X_random)
+    sigma = sigmaKov(Xsin, lbda, kov)
     L = np.linalg.cholesky(sigma)
-    Lt = L.T
+    #Lt = L.T
     fig, ax = plt.subplots()
     plt.xlabel("X")
     plt.ylabel("Valeur de la variable aléatoire")
     plt.title(r"Réalisations de GP pour $\lambda$ = " + str(lbda))
-    for i in range(5):
-        g = np.random.normal(0, 1, size=N)
+    for i in range(2):
+        g = np.random.normal(size=N)
         
-        z = Lt @ g 
+        z = L @ g 
         #display(z.round(2))
-        ax.plot(X, z, label = f"Sim {i}")
+        ax.plot(X_random, z, label = f"Sim {i}")
+    ax.plot(X_random, Xsin, label = "Sinus")
     plt.legend()
     plt.grid()
     plt.show()
@@ -66,6 +71,38 @@ def draw_uniform_in_intervals(N):
     return numbers
 
 
+def neglikelihood(lbda, Z):
+    sigma = sigmaKov(Z, lbda, kov)
+    L = np.linalg.cholesky(sigma)
+    d = len(Z)
+    Z = Z.reshape(1, d)
+    #print(f"Z : {Z}")
+    Zt = Z.T
+    #print(f"Zt : {Zt}")
+    a = d * np.log(2 * np.pi)
+    #print(f"a = {a}")
+    detsig = np.linalg.det(L)
+    invsig = np.linalg.inv(L)
+    b = 0.5 * np.log(detsig)
+    #print(f"b = {b} : 0.5 * np.log(np.linalg.det(sigma)) with detsig = {detsig}")
+    c = (0.5 * Z @ invsig @ Zt).flatten()[0]
+    print(f"Pour lambda = {lbda}, Déterminant de sigma : {detsig} et resultat = {a+b+c}")
+    return a + b + c
 
-def neglikelihood(sigma, lbda, N):
-    z = np.sin(4 * np.pi * draw_uniform_in_intervals(N))
+
+def logf(lambda_, x, z, n):
+    lambda_ = float(lambda_) + 0.01 if lambda_ == 0 else lambda_
+    # L_matrix = lu(covariance_matrix(x, lambda_))[1]
+    # logdetS = slogdet(L_matrix.T)[1]
+    # invS = L_matrix  # inv(L_matrix)
+    sigma = sigmaKov(x, lambda_, kov)
+    # L = lu(sigma)[1]
+    logdetS = np.linalg.slogdet(sigma)[1]
+    # logdetS = log(det(sigma))
+    invS = np.linalg.inv(sigma)
+    # U = lu(sigma)[1]
+    # g = np.random.normal(size=x.shape[0])
+    # mean = U @ g
+    # mean = np.mean(x)
+    # print(logdetS)
+    return 0.5 * (n * np.log(2 * np.pi) + logdetS + z.T @ invS @ z)
