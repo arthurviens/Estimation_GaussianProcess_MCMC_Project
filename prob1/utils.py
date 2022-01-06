1224a945464becae912b91c15068f21dc7eeaa59
Arthur Viens <arthur.viens@mines-paristech.fr>
2022-01-06 19:28:29 +0100

    Problème 1 terminé

--- prob1/utils.py
+++ prob1/utils.py
@@ -39,9 +39,8 @@ def estimators_likelihood(X):
     m = alpha * (np.log(len(X)) - np.log(np.sum(np.exp(-X/alpha))))
     return m, alpha
 
-
 def test_m_convergence(m, method, alpha=1, N = 1000):
-    start = 5
+    start = 20
     pas = 5
     # Moments
     if method == "moments":
@@ -53,7 +52,8 @@ def test_m_convergence(m, method, alpha=1, N = 1000):
         fig, ax = plt.subplots()
         m_hat = np.asarray(m_estimates)[-20:].mean()
 
-        ax.plot(range(start, N, pas), m_estimates, color="yellow", marker='.', linestyle="", label = r"$\hat{}$ = {}".format("m", m_hat.round(3)))
+        ax.plot(range(start, N, pas), m_estimates, color="green", marker='o', linestyle="", \
+            label = r"$\hat{}$ = {}".format("m", m_hat.round(3)), ms=100./fig.dpi)
         mi, ma = plt.xlim()
         ax.hlines(m, mi, ma, color="red", linestyle="dashed", label="m = "+str(m), zorder=10)
         plt.grid()
@@ -72,7 +72,8 @@ def test_m_convergence(m, method, alpha=1, N = 1000):
         fig, ax = plt.subplots()
         m_hat = np.asarray(m_estimates)[-20:].mean()
 
-        ax.plot(range(start, N, pas), m_estimates, color="yellow", marker='.', linestyle="", label = r"$\hat{}$ = {}".format("m", m_hat.round(3)))
+        ax.plot(range(start, N, pas), m_estimates, color="yellow", marker='o', linestyle="", \
+            label = r"$\hat{}$ = {}".format("m", m_hat.round(3)), ms=100./fig.dpi)
         mi, ma = plt.xlim()
         ax.hlines(m, mi, ma, color="red", linestyle="dashed", label="m = "+str(m), zorder=10)
         plt.grid()
@@ -81,11 +82,34 @@ def test_m_convergence(m, method, alpha=1, N = 1000):
         plt.ylabel("Valeur du paramètre")
         plt.legend()
         plt.show()
-
-
+    if method == "both":
+        m_estimates_moment = []
+        m_estimates_likeli = []
+        for n in range(start, N, pas):
+            samples = scipy.stats.gumbel_r.rvs(loc=m, scale=alpha, size=n)
+            m_hat_m, alpha_hat = estimators_moment(samples)
+            m_hat_l, alpha_hat = estimators_likelihood(samples)
+            m_estimates_moment.append(m_hat_m)
+            m_estimates_likeli.append(m_hat_l)
+        fig, ax = plt.subplots()
+        m_hat_m = np.asarray(m_estimates_moment)[-20:].mean()
+        m_hat_l = np.asarray(m_estimates_likeli)[-20:].mean()
+        
+        ax.plot(range(start, N, pas), m_estimates_moment, color="green", marker='o', ms=60./fig.dpi, \
+            linestyle="", label = r"$\hat{}$ moment = {}".format("m", m_hat_m.round(3)))
+        ax.plot(range(start, N, pas), m_estimates_likeli, color="yellow", marker='o', ms=60./fig.dpi, \
+            linestyle="", label = r"$\hat{}$ likeli = {}".format("m", m_hat_l.round(3)))
+        mi, ma = plt.xlim()
+        ax.hlines(m, mi, ma, color="red", linestyle="dashed", label="m = "+str(m), zorder=10)
+        plt.grid()
+        plt.title("Convergence de l'estimateur de m des moments et vraisemblance")
+        plt.xlabel("Taille de l'échantillon")
+        plt.ylabel("Valeur du paramètre")
+        plt.legend()
+        plt.show()
 
 def test_alpha_convergence(alpha, method, m=0, N = 1000):
-    start = 5
+    start = 20
     pas = 5
     if method == "moments":
         alpha_estimates = []
@@ -96,7 +120,8 @@ def test_alpha_convergence(alpha, method, m=0, N = 1000):
         fig, ax = plt.subplots()
         alpha_hat = np.asarray(alpha_estimates)[-20:].mean()
 
-        ax.plot(range(start, N, pas), alpha_estimates, color="blue", marker='x', linestyle="", label=r"$\hat{\alpha}$ = " + str(alpha_hat.round(3)))
+        ax.plot(range(start, N, pas), alpha_estimates, color="green", marker='x', linestyle="", \
+            label=r"$\hat{\alpha}$ = " + str(alpha_hat.round(3)), ms=100./fig.dpi)
         mi, ma = plt.xlim()
         ax.hlines(alpha, mi, ma, color="black", linestyle="dashed", label=r"$\alpha$ = " + str(alpha), zorder=10)
         plt.grid()
@@ -114,11 +139,37 @@ def test_alpha_convergence(alpha, method, m=0, N = 1000):
         fig, ax = plt.subplots()
         alpha_hat = np.asarray(alpha_estimates)[-20:].mean()
 
-        ax.plot(range(start, N, pas), alpha_estimates, color="blue", marker='x', linestyle="", label=r"$\hat{\alpha}$ = " + str(alpha_hat.round(3)))
+        ax.plot(range(start, N, pas), alpha_estimates, color="yellow", marker='x', linestyle="", \
+            label=r"$\hat{\alpha}$ = " + str(alpha_hat.round(3)), ms=100./fig.dpi)
         mi, ma = plt.xlim()
         ax.hlines(alpha, mi, ma, color="black", linestyle="dashed", label=r"$\alpha$ = " + str(alpha), zorder=10)
         plt.grid()
         plt.title(r"Convergence de l'estimateur de $\alpha$ de la vraisemblance")
+        plt.xlabel("Taille de l'échantillon")
+        plt.ylabel("Valeur du paramètre")
+        plt.legend()
+        plt.show()
+    if method == "both":
+        alpha_estimates_moment = []
+        alpha_estimates_likeli = []
+        for n in range(start, N, pas):
+            samples = scipy.stats.gumbel_r.rvs(loc=m, scale=alpha, size=n)
+            m_hat_m, alpha_hat_m = estimators_moment(samples)
+            m_hat_l, alpha_hat_l = estimators_likelihood(samples)
+            alpha_estimates_moment.append(alpha_hat_m)
+            alpha_estimates_likeli.append(alpha_hat_l)
+        fig, ax = plt.subplots()
+        alpha_hat_m = np.asarray(alpha_estimates_moment)[-20:].mean()
+        alpha_hat_l = np.asarray(alpha_estimates_likeli)[-20:].mean()
+
+        ax.plot(range(start, N, pas), alpha_estimates_moment, color="green", marker='o', ms=60./fig.dpi, \
+            linestyle="", label = r"$\hat{}$ moment = {}".format(r"\alpha", alpha_hat_m.round(3)))
+        ax.plot(range(start, N, pas), alpha_estimates_likeli, color="yellow", marker='o', ms=60./fig.dpi, \
+            linestyle="", label = r"$\hat{}$ likeli = {}".format(r"\alpha", alpha_hat_l.round(3)))
+        mi, ma = plt.xlim()
+        ax.hlines(alpha, mi, ma, color="black", linestyle="dashed", label=r"$\alpha$ = "+str(alpha), zorder=10)
+        plt.grid()
+        plt.title(r"Convergence de l'estimateur de $\alpha$ des moments et vraisemblance")
         plt.xlabel("Taille de l'échantillon")
         plt.ylabel("Valeur du paramètre")
         plt.legend()
